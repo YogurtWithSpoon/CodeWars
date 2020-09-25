c6676c3a312438e7839728050cf71d0b025f2770
Egor Kochkin <egurt96@yandex.ru>
2020-09-26 00:52:51 +0300

    6 kyu

--- mexican wave.js
+++ mexican wave.js
@@ -1,5 +1,23 @@
 function wave(str){
-    // Code here
+    let countLetters = str.length
+    let i = 0;
+    let blank = []
+    while(i < countLetters){
+        blank.push(str);
+        i++;
+    }
+    let index = 0;
+
+    let finish = blank.map(item => {
+        if(item[index] !== ' '){
+            let result = item.slice(0,index) + item[index].toUpperCase() + item.slice(index+1)
+            index++;
+            return result;
+        } else {
+            index++;
+        }
+    })
+    return finish.filter(a => a)
 }
 
 
