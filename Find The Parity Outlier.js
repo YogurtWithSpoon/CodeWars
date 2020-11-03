c3ab87fff88d4be4d72c4a6d99b01b7d664c6294
Egor Kochkin <egurt96@yandex.ru>
2020-11-03 18:26:37 +0300

    6 kyu

--- Find The Parity Outlier.js
+++ Find The Parity Outlier.js
@@ -1,3 +1,8 @@
+function findOutlier(array){
+  return array.filter(item => item % 2 == 0).length == 1 ? array.filter(item => item % 2 == 0)[0] : array.filter(item => item % 2 !== 0)[0]
+}
+
+
 console.log(findOutlier([0, 1, 2])) //, 1)
 console.log(findOutlier([1, 2, 3])) //, 2)
 console.log(findOutlier([2,6,8,10,3])) //, 3)
